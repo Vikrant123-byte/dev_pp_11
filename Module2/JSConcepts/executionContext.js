f2ca1aff8f6fd61441fa98ca8ab51524e570c9db
vikrant <the.vikrantjionly@gmail.com>
2021-05-21 14:57:20 +0530

    javascript.concepts

---
+++ Module2/JSConcepts/executionContext.js
@@ -0,0 +1,13 @@
+console.log("start");
+var name = "steve";
+
+function getname(name){
+    var name ="rogers";
+
+
+    }
+var fullName =getname("steve");
+var newname =getname("tony");
+
+console.log(fullName);
+console.log(newName);
---
+++ Module2/JSConcepts/lecVsVar.js
@@ -0,0 +1,13 @@
+//let - block scoped variable
+//const - block scop
+//undefine case
+console.log(a);
+var a ="steve";
+console.log(a);
+//hosting- undefine
+//let,cost me bi hostinghoti ha a-temporialy dead zone me chela jata ha ,cannot access before initilition
+//we can not access variable in temporily dead zone !!
+//not define 
+//console.log(a);
+//can not access case 
+//let,const ke program me 
